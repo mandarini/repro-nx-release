1ec7cecd7c84ef1f7fe0436b3f94e3bbf6ef0271
Katerina Skroumpelou <sk.katherine@gmail.com>
2026-04-02 17:20:08 +0300

    feat: some nice feature to tests

--- packages/core/main/src/index.js
+++ packages/core/main/src/index.js
@@ -2,3 +2,6 @@ export * from './lib/main.js';
 
 console.log('main log hello thasdfsdere');
 console.log('main log hello thasdfsdere');
+console.log('main log hello thasdfsdere');
+console.log('main log hello thasdfsdere');
+console.log('main log hello thasdfsdere');
